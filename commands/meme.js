401f5069d0e86de6b742617ed346f43463c676f1
JohnGameRage <johnmichadasis@gmail.com>
2021-05-07 00:19:58 +0300

    fix

--- commands/meme.js
+++ commands/meme.js
@@ -1,25 +1,25 @@
 
 const { MessageEmbed } = require('discord.js')
-const { randomPuppy } = require("random-puppy");
+const randomPuppy = require("random-puppy");
 
 module.exports = {
   name: "meme",
   description: " ",
 
   async run(client, message, args) {
-    message.channel.send("This command is not maintained anymore.")
-    //if(!message.channel.nsfw)return message.channel.send(message.author + " This Command can be used ONLY in a nsfw channel.")
-//const subReddits = ["dankmemes", "meme", "me_irl", "im14andthisisdeep"];
-//const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    //message.channel.send("This command is not maintained anymore.")
+    if(!message.channel.nsfw)return message.channel.send(message.author + " This Command can be used ONLY in a nsfw channel.")
+const subReddits = ["dankmemes", "meme", "me_irl", "im14andthisisdeep"];
+const random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
-//const img = await randomPuppy(random);
-// memebed = new MessageEmbed()
-//    .setColor("RANDOM")
-//    .setImage(img)
-//    .setTitle(`From r/${random}`)
-//    .setURL(`https://reddit.com/r/${random}`);
-//
-//message.channel.send(memebed);
+const img = await randomPuppy(random);
+ memebed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setImage(img)
+   .setTitle(`From r/${random}`)
+    .setURL(`https://reddit.com/r/${random}`);
+
+message.channel.send(memebed);
 console.log(`KEWL BOT    | MEME COMMAND USED IN ${message.guild.name}`)
   }
 }
